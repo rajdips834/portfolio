bdf087839d4d20ec7f7d4b5f51962cab0c2a990d
Rajdip Sinha <rajdipsinha@Anirbans-MacBook-Pro.local>
2023-08-19 12:10:13 +0530

    Pre animated background

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+export default function Navbar() {
+  return (
+    <nav className="fixed w-full">
+      <ul className="flex justify-between space-x-4 text-xl backdrop-filter backdrop-blur-lg text-slate-50">
+        {" "}
+        <li>
+          <a
+            href="/"
+            className="px-2 py-1 rounded hover:bg-gray-200 active:text-green-600"
+          >
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="/about" className="px-2 py-1 rounded hover:bg-gray-200">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="/contact" className="px-2 py-1 rounded hover:bg-gray-200">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,13 +1,26 @@
 import "./About.css";
+import { useRef, useEffect, useState } from "react";
+import "./About.css";
 import { motion } from "framer-motion";
 import React from "react";
 import AnimatedTextCharacter from "../../components/typewriter/AnimatedTextCharacter";
 import AnimatedTextWord from "../../components/typewriter/AnimatedTextWord";
+
 export default function About() {
+  const myRef = useRef();
+  const [myElementisvisible, setMyElementisvisible] = useState(false);
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      console.log(entry);
+    });
+    observer.observe(myRef.current);
+  }, []);
+
   return (
     <div>
-      <h1 className=" bg-amber-100 relative top-[90vh]">
-        <AnimatedTextWord text="whah theee aaaa"></AnimatedTextWord>
+      <h1 ref={myRef} className=" about bg-amber-100 relative top-[90vh]">
+        <AnimatedTextWord text="Meet Rajdip, the React Rockstart."></AnimatedTextWord>
       </h1>
     </div>
   );
