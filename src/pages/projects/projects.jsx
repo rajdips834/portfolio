bdd9d904c65165f2558131403b4d1b718207e602
Rajdip Sinha <rajdipsinha@Anirbans-MacBook-Pro.local>
2023-09-22 02:13:07 +0530

    Broke the site into i like to build and build what

--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -1,9 +1,13 @@
 export default function Projects(props) {
-  return (
-    <>
-      <div className="relative flex justify-center pb-4 text-4xl text-slate-100 top-[300vh] sm:text-4xl md:text-6xl lg:text-8xl">
-        I like to build stuff.
-      </div>
-    </>
-  );
+  const projectLinks = [
+    {
+      name: "Portfolio",
+      link: "https://github.com/rajdips834/portfolio",
+    },
+    {
+      name: "Portfolio",
+      link: "https://github.com/rajdips834/portfolio",
+    },
+  ];
+  return <></>;
 }
