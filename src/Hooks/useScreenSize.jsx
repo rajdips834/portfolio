0ef3bd33241f0806d815902a689e8ee309e5ea90
Rajdip Sinha <rajdipsinha@Anirbans-MacBook-Pro.local>
2023-09-21 23:44:22 +0530

    Design changed

---
+++ src/Hooks/useScreenSize.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+export default function useScreenSize() {
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  return screenSize;
+}
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,46 +1,60 @@
-import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-import { React, useRef } from "react";
+import { motion, useInView, useAnimation } from "framer-motion";
+import { React, useEffect, useRef } from "react";
 import "./About.css";
 import AnimatedTextWord from "../../components/typewriter/AnimatedTextWord";
 
 export default function About() {
-  const [myRef, inView] = useInView();
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+  const mainControls = useAnimation();
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    }
+  }, [isInView]);
+
   return (
-    <div className="text-slate-100">
-      <h1
-        ref={myRef}
-        className="relative justify-center pb-4 text-4xl align-middle about sm:text-4xl md:text-6xl lg:text-8xl"
+    <>
+      <motion.h1
+        className="relative flex justify-center pb-4 text-4xl text-slate-100 about sm:text-4xl md:text-6xl lg:text-8xl"
+        variants={{
+          hidden: { opacity: 0 },
+          visible: { opacity: 1, transition: { delay: 0.5 } },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 1.5, delay: 0.5, type: "tween", damping: 12 }}
       >
-        <AnimatedTextWord text="Meet Rajdip, the React Whiz." />
-      </h1>
-      <div className="top-[90vh] relative gap-10 justify-center flex flex-wrap">
+        I like to build things.
+      </motion.h1>
+
+      <motion.h1
+        className="relative flex justify-center pb-4 text-4xl text-slate-100 about sm:text-4xl md:text-6xl lg:text-8xl"
+        variants={{
+          hidden: { opacity: 0 },
+          visible: { opacity: 1, y: "700%", transition: { delay: 0.5 } },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 1.5, delay: 0.5, type: "tween", damping: 12 }}
+      >
+        Build what?
+      </motion.h1>
+      <div
+        ref={ref}
+        className="top-[90vh] relative gap-10 justify-center flex flex-wrap"
+      >
         <motion.div
-          initial={{ x: "-100%", opacity: 0 }}
-          animate={{ x: "0%", opacity: 1 }}
-          transition={{ duration: 1.5, delay: 0.5, type: "tween", damping: 12 }}
-          className="relative w-full pt-32w` text-xl md:text-2xl lg:text-2xl xl:text-2xl md:w-1/2"
-        >
-          Rajdip is a React developer with an undeniable talent for creating
-          jaw-dropping applications. He’s worked with leading companies to
-          elevate their web development game.
-        </motion.div>
-        <motion.div
-          initial={{ x: "120%", y: "110%" }}
-          animate={{ x: "0%", y: "110%" }}
-          transition={{
-            duration: 1,
-            delay: 0.5,
-            type: "intertia",
-            damping: 1,
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1, transition: { delay: 1 } },
           }}
-          className="relative w-full p-4 text-xl md:text-2xl lg:text-2xl xl:text-2xl md:w-1/2"
-        >
-          Out with the old, in with the new! Rajdip takes ordinary web apps and
-          transforms them into masterpieces. Are you ready to dive into a world
-          of impeccable design and functionality? Buckle up and join the ride!
-        </motion.div>
+          initial="hidden"
+          animate={mainControls}
+          transition={{ duration: 1.5, delay: 0.5, type: "tween", damping: 12 }}
+          className=" text-slate-100 relative w-full pt-[2rem] text-xl md:text-2xl lg:text-2xl xl:text-2xl md:w-1/2"
+        ></motion.div>
       </div>
-    </div>
+    </>
   );
 }
